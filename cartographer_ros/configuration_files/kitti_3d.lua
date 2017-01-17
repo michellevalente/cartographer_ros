2bc788caece5c1d69397d0f6a1d563a2cf02cb67
Kent Sommer <kent.sommer13@gmail.com>
2017-01-17 10:23:25 +0900

    Fixed

--- cartographer_ros/configuration_files/kitti_3d.lua
+++ cartographer_ros/configuration_files/kitti_3d.lua
@@ -17,7 +17,7 @@ include "map_builder.lua"
 options = {
   map_builder = MAP_BUILDER,
   map_frame = "map",
-  tracking_frame = "imu",
+  tracking_frame = "imu_link",
   published_frame = "base_link",
   odom_frame = "odom",
   provide_odom_frame = true,
